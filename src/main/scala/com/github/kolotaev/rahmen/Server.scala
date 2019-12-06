5bc095f91ec768a6f6147ceec7b7637834f63f3e
kolotaev <kolotaev@tut.by>
2019-12-06 15:59:20 +0300

    Add types for implicits

--- src/main/scala/com/github/kolotaev/rahmen/Server.scala
+++ src/main/scala/com/github/kolotaev/rahmen/Server.scala
@@ -5,16 +5,17 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
 
 object Server {
-  def main(args: Array[String]) {
-
-    implicit val system = ActorSystem("my-system")
-    implicit val materializer = ActorMaterializer()
+  def main(args: Array[String]): Unit = {
+    implicit val system: ActorSystem = ActorSystem("my-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
     // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
     val route =
       path("hello") {
