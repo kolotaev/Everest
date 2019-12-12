3ff67085e49a8e1d9230d5eb5423115b658cb0e2
kolotaev <kolotaev@tut.by>
2019-12-12 18:47:08 +0300

    Add return types on storage methods

--- src/main/scala/com/github/kolotaev/everest/DataStorage.scala
+++ src/main/scala/com/github/kolotaev/everest/DataStorage.scala
@@ -1,5 +1,6 @@
 package com.github.kolotaev.everest
 
+import scala.concurrent.Future
 
 /**
  * Generic interface for particular DB backends.
@@ -8,25 +9,25 @@ trait DataStorage {
   /**
    * Find resource items that match a lookup query.
    */
-  def find(resource: String, request: Request, lookup: Lookup, doCount: Boolean = false)
+  def find(resource: String, request: Request, lookup: Lookup, doCount: Boolean = false): Future[Items]
 
   /**
    * Insert resource items into a data storage.
    */
-  def insert(resource: String, items: Items)
+  def insert(resource: String, items: Items): Future[Item]
 
   /**
    * Update particular resource item.
    */
-  def update(resource: String, item: Item, old: Item)
+  def update(resource: String, item: Item, old: Item): Future[Boolean]
 
   /**
    * Delete resource item
    */
-  def delete(resource: String, item: Item)
+  def delete(resource: String, item: Item): Future[String]
 
   /**
    * Remove resource items from the storage that match a lookup query.
    */
-  def clear(resource: String, lookup: Lookup)
+  def clear(resource: String, lookup: Lookup): Future[Long]
 }
