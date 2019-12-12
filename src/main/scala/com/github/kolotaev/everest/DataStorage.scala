a4cc0e3023b3e8d4b4a8a969a4a0efab7b2ccd06
kolotaev <kolotaev@tut.by>
2019-12-12 18:35:45 +0300

    Add some docs on storage methods

--- src/main/scala/com/github/kolotaev/everest/DataStorage.scala
+++ src/main/scala/com/github/kolotaev/everest/DataStorage.scala
@@ -1,10 +1,32 @@
 package com.github.kolotaev.everest
 
 
+/**
+ * Generic interface for particular DB backends.
+ */
 trait DataStorage {
+  /**
+   * Find resource items that match a lookup query.
+   */
   def find(resource: String, request: Request, lookup: Lookup, doCount: Boolean = false)
+
+  /**
+   * Insert resource items into a data storage.
+   */
   def insert(resource: String, items: Items)
+
+  /**
+   * Update particular resource item.
+   */
   def update(resource: String, item: Item, old: Item)
+
+  /**
+   * Delete resource item
+   */
   def delete(resource: String, item: Item)
+
+  /**
+   * Remove resource items from the storage that match a lookup query.
+   */
   def clear(resource: String, lookup: Lookup)
 }
