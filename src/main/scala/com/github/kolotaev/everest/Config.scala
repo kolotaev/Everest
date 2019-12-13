9604744a6ae060a35a5e05798c983f152b92fb49
kolotaev <kolotaev@tut.by>
2019-12-13 17:29:58 +0300

    Add configuration bits

--- src/main/scala/com/github/kolotaev/everest/Config.scala
+++ src/main/scala/com/github/kolotaev/everest/Config.scala
@@ -1,17 +1,37 @@
 package com.github.kolotaev.everest
 
 class Config {
-  val database: Option[Database] = None
-  val schema: Option[Schema] = None
+  val dataSources: Seq[DataSource] = List.empty
+  val resources: Seq[Resource] = List.empty
 }
 
-class Database {
-  val host: String = ""
-  val port: Int = 0
-  val database: String = ""
-  val DSN: String = ""
+class DataSource(
+  val host: String = "",
+  val port: Int = 0,
+  val databaseName: String = "",
+  val DSN: String = "",
+  val User: String = "",
+  val Password: String = ""
+)
+
+class Resource(
+  val schema: Schema,
+  val title: Option[String],
+  val additionalLookup: Map[String, String],
+  val cacheExpires: String,
+  val cache: CacheOptions,
+  val methods: HTTPMethod.Value
+//  val dataSource: DataSource
+)
+
+class CacheOptions(val control: String, val expires: String)
+
+class Schema(val description: String, fields: Fields)
+
+object HTTPMethod extends Enumeration {
+  val GET, POST, DELETE, PUT = Value
 }
 
-class Schema {
+trait FieldRequirement
 
-}
+class Field(val )
--- src/main/scala/com/github/kolotaev/everest/package.scala
+++ src/main/scala/com/github/kolotaev/everest/package.scala
@@ -5,4 +5,8 @@ package object everest {
 
   // todo: do we use it?
   type Links = List[Link]
+
+//  type Field = Map[String, FieldRequirement]
+
+  type Fields = List[Field]
 }
